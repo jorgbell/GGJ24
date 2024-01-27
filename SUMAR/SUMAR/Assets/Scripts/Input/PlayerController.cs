fe94dd10f91af1c0888f0a217dbeb8497d59341c
jorgbell <jorgbell@ucm.es>
2024-01-27 13:49:26 +0100

    GameManager, ArenaManager, PlayerController, creada gestion de conexion y desconexion en caliente + input separado segun el ID de la conexion

--- SUMAR/SUMAR/Assets/Scripts/Input/PlayerController.cs
+++ SUMAR/SUMAR/Assets/Scripts/Input/PlayerController.cs
@@ -1,29 +1,45 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 //enum para gestionar los tipos de inputs digitales (botones encendido-apagado)
-public enum INPUTACTIONS { ATTACK, CATCH, THROW, DASH, TAUNT, PAUSE};
+public enum INPUTACTIONS { ATTACK, CATCH, THROW, DASH, TAUNT, PAUSE };
 
 public class PlayerController : MonoBehaviour
 {
     public PlayerActions playerInput;
     private Vector3 axisvalue = new Vector3();
     private Queue<INPUTACTIONS> inputQueue = new Queue<INPUTACTIONS>();
-
+    private int deviceID = -1;
+    public int LinkedDeviceID { get { return deviceID; } }
+    [SerializeField]
+    private int playerID = 1;
 
     private void Awake()
     {
         playerInput = new PlayerActions();
         // por cada input que esté en el enum hacer esto
-        playerInput.PlayerActionMap.Attack.performed += ctx => EnqueueActionInput(INPUTACTIONS.ATTACK);
-        playerInput.PlayerActionMap.CatchBall.performed += ctx => EnqueueActionInput(INPUTACTIONS.CATCH);
-        playerInput.PlayerActionMap.ThrowBall.performed += ctx => EnqueueActionInput(INPUTACTIONS.THROW);
-        playerInput.PlayerActionMap.Dash.performed += ctx => EnqueueActionInput(INPUTACTIONS.DASH);
-        playerInput.PlayerActionMap.Taunt.performed += ctx => EnqueueActionInput(INPUTACTIONS.TAUNT);
-        playerInput.PlayerActionMap.Pause.performed += ctx => EnqueueActionInput(INPUTACTIONS.PAUSE);
+        playerInput.PlayerActionMap.Attack.performed += ctx => EnqueueActionInput(ctx, INPUTACTIONS.ATTACK);
+        playerInput.PlayerActionMap.CatchBall.performed += ctx => EnqueueActionInput(ctx, INPUTACTIONS.CATCH);
+        playerInput.PlayerActionMap.ThrowBall.performed += ctx => EnqueueActionInput(ctx, INPUTACTIONS.THROW);
+        playerInput.PlayerActionMap.Dash.performed += ctx => EnqueueActionInput(ctx, INPUTACTIONS.DASH);
+        playerInput.PlayerActionMap.Taunt.performed += ctx => EnqueueActionInput(ctx, INPUTACTIONS.TAUNT);
+        playerInput.PlayerActionMap.Pause.performed += ctx => EnqueueActionInput(ctx, INPUTACTIONS.PAUSE);
+    }
+
+    public void LinkDevice(int uniqueID)
+    {
+        if (uniqueID == -1)
+            Debug.Log("Jugador " + playerID + " desconectado, ID desconectado: " + deviceID);
+        else
+        {
+            deviceID = uniqueID;
+            Debug.Log("Jugador " + playerID + " conectado con mando " + deviceID);
+        }
+
     }
 
     private void OnEnable()
@@ -39,7 +55,7 @@ public class PlayerController : MonoBehaviour
     private void Update()
     {
         INPUTACTIONS catchedInput;
-        while(inputQueue.TryDequeue(out catchedInput))
+        while (inputQueue.TryDequeue(out catchedInput))
         {
             Debug.Log("reading input queue...");
             switch (catchedInput)
@@ -68,16 +84,21 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    public void EnqueueActionInput(INPUTACTIONS input)
+    public void EnqueueActionInput(InputAction.CallbackContext ctx, INPUTACTIONS input)
     {
-        inputQueue.Enqueue(input);
+        if (ctx.control.device.deviceId == deviceID)
+            inputQueue.Enqueue(input);
     }
 
     public void OnMovement(InputAction.CallbackContext ctx)
     {
-        Debug.Log("Axis movement change");
-        Debug.Log(axisvalue);
-        axisvalue = ctx.ReadValue<Vector3>();
+        if (ctx.control.device.deviceId == deviceID)
+        {
+            Debug.Log("Axis movement change");
+            Debug.Log(axisvalue);
+            axisvalue = ctx.ReadValue<Vector3>();
+
+        }
     }
 
 }
